5d67e0539f28d85c1757046a744fa6f37be78c7e
Kamal <kamalpluto@gmail.com>
2019-10-02 23:50:50 +0530

    Add a new route to display lead details

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ module.exports = {
     "database": "express-mvp-db",
     "host": "127.0.0.1",
     "dialect": "postgres",
-    "port": 5432
+    "port": 5432,
     "operatorsAliases": false
   },
   "test": {
